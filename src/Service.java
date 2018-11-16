ee5e2b0afafd84653c4dd79e00ee0c9e15616cb5
Yuta2049 <vinci2049@gmail.com>
2018-11-16 11:17:34 +0400

    Calculate correct

--- src/Service.java
+++ src/Service.java
@@ -28,9 +28,9 @@ public class Service {
 
         String number = "";
         int position = 0;
-        for (int i = indexOfSign-1; i>=0; i--) {
+        for (int i = indexOfSign - 1; i >= 0; i--) {
             char currentChar = expression.charAt(i);
-            if (Character.isDigit(currentChar) || currentChar == '.' || currentChar == ',') {
+            if ((Character.isDigit(currentChar) || currentChar == '.' || currentChar == ',') || (currentChar == '-' && i == 0)) {
                 number = expression.charAt(i) + number;
                 position = i;
             } else {
@@ -45,6 +45,7 @@ public class Service {
             numberDouble = Double.parseDouble(number);
         } catch (Exception e) {
             System.out.println("Не получилось преобразовать текст " + number + " в число");
+            throw new NumberFormatException();
         }
 
 
@@ -59,13 +60,12 @@ public class Service {
 
         String number = "";
         int position = 0;
-        for (int i = indexOfSign+1; i<expression.length(); i++) {
+        for (int i = indexOfSign + 1; i < expression.length(); i++) {
             char currentChar = expression.charAt(i);
             if (Character.isDigit(currentChar) || currentChar == '.' || currentChar == ',') {
                 number = number + expression.charAt(i);
                 position = i;
-            }
-            else {
+            } else {
                 break;
             }
         }
@@ -75,8 +75,9 @@ public class Service {
         double numberDouble = 0.0;
         try {
             numberDouble = Double.parseDouble(number);
-        } catch (Exception e){
-            System.out.println("Не получилось преобразовать текст "+number+" в число");
+        } catch (Exception e) {
+            System.out.println("Не удалось преобразовать текст " + number + " в число");
+            throw new NumberFormatException();
         }
 
         PositionWithResult positionWithResult = new PositionWithResult();
@@ -89,23 +90,20 @@ public class Service {
 
     private double calcExpressionWithoutParenthesis(String expression) {
 
-        //System.out.println("Выражение до замены: "+expression);
-
         double result = 0;
 
-        while (Math.max(expression.indexOf("*"), expression.indexOf("/")) > 0)
-        {
+        while (Math.max(expression.indexOf("*", 1), expression.indexOf("/", 1)) > 0) {
 
-            int indexMinus = expression.indexOf("/");
-            int indexPlus = expression.indexOf("*");
+            int indexMinus = expression.indexOf("/", 1);
+            int indexPlus = expression.indexOf("*", 1);
 
             int indexOfSign = 0;
             String sign = "Plus";
 
-            if ((indexMinus >= 0 && indexPlus >= 0 && indexMinus < indexPlus) || (indexMinus >= 0 && indexPlus < 0)) {
+            if ((indexMinus > 0 && indexPlus > 0 && indexMinus < indexPlus) || (indexMinus > 0 && indexPlus < 0)) {
                 indexOfSign = indexMinus;
                 sign = "Minus";
-            } else if ((indexMinus >= 0 && indexPlus >= 0 && indexMinus > indexPlus) || (indexPlus >= 0 && indexMinus < 0)) {
+            } else if ((indexMinus > 0 && indexPlus > 0 && indexMinus > indexPlus) || (indexPlus > 0 && indexMinus < 0)) {
                 indexOfSign = indexPlus;
                 sign = "Plus";
             }
@@ -119,7 +117,7 @@ public class Service {
                 result = leftResult.getResult() * rightResult.getResult();
             }
 
-           // System.out.println("Левый символ: " + leftResult.getPosition());
+            // System.out.println("Левый символ: " + leftResult.getPosition());
             //System.out.println("Правый символ: " + rightResult.getPosition());
 
             expression = expression.substring(0, leftResult.getPosition()) + Double.valueOf(result) + expression.substring(rightResult.getPosition() + 1);
@@ -129,22 +127,20 @@ public class Service {
         }
 
 
-
         // тут цикл пока не останется знаков действий
         //while (Math.max(expression.indexOf("+"), expression.indexOf("-"))+ Math.max(expression.indexOf("*"), expression.indexOf("/")) > -2)
-        while (Math.max(expression.indexOf("+"), expression.indexOf("-")) > 0)
-        {
+        while (Math.max(expression.indexOf("+", 1), expression.indexOf("-", 1)) > 0) {
 
-            int indexMinus = expression.indexOf("-");
-            int indexPlus = expression.indexOf("+");
+            int indexMinus = expression.indexOf("-", 1);
+            int indexPlus = expression.indexOf("+", 1);
 
             int indexOfSign = 0;
             String sign = "Plus";
 
-            if ((indexMinus >= 0 && indexPlus >= 0 && indexMinus < indexPlus) || (indexMinus >= 0 && indexPlus < 0)) {
+            if ((indexMinus > 0 && indexPlus > 0 && indexMinus < indexPlus) || (indexMinus > 0 && indexPlus < 0)) {
                 indexOfSign = indexMinus;
                 sign = "Minus";
-            } else if ((indexMinus >= 0 && indexPlus >= 0 && indexMinus > indexPlus) || (indexPlus >= 0 && indexMinus < 0)) {
+            } else if ((indexMinus > 0 && indexPlus > 0 && indexMinus > indexPlus) || (indexPlus > 0 && indexMinus < 0)) {
                 indexOfSign = indexPlus;
                 sign = "Plus";
             }
@@ -176,34 +172,30 @@ public class Service {
         double result = 0.0;
 
         // тут цикл пока не останется скобок
-        while (Math.max(expression.indexOf("("), expression.indexOf(")")) > 0)
-        {
+        while (Math.max(expression.indexOf("("), expression.indexOf(")")) > 0) {
             //String regex = "\\(.[^\\(\\)]*\\)";
             String regex = "[\\(][^\\(\\)]*+[\\)]";
             Pattern pattern = Pattern.compile(regex);
 
             Matcher match = pattern.matcher(expression);
 
-            int matchCounter = 0;
-            System.out.println("bu");
-            while (match.find()){
-                //matchCounter++;
-                System.out.println("start(): " + match.start());
-                System.out.println("end(): " + match.end());
-                //System.out.println("Number of match: " + matchCounter);
+            while (match.find()) {
 
-                //expression = calcExpressionWithoutParenthesis(expression);
-                System.out.println("ВЫР: " + expression.substring(match.start(), match.end()));
-
-                //expression = expression.substring(0, match.start()) + Double.valueOf(calcExpressionWithoutParenthesis(expression.substring(match.start(), match.end()))) + expression.substring(match.end());
-                //expression = expression.replaceFirst(regex, String.valueOf(calcExpressionWithoutParenthesis(expression.substring(match.start(), match.end()))));
                 expression = match.replaceFirst(String.valueOf(calcExpressionWithoutParenthesis(expression.substring(match.start(), match.end()))));
 
                 System.out.println("Выражение после замены: " + expression);
             }
         }
 
-        result = calcExpressionWithoutParenthesis(expression);
+//        if ((Math.max(expression.indexOf("*", 1), expression.indexOf("/", 1)) > 0) ||
+//           (Math.max(expression.indexOf("+", 1), expression.indexOf("-", 1)) > 0)) {
+
+        try {
+            result = Double.valueOf(expression);
+
+        } catch (NumberFormatException e) {
+            result = calcExpressionWithoutParenthesis(expression);
+        }
 
         return result;
     }
--- src/Starter.java
+++ src/Starter.java
@@ -4,6 +4,7 @@ public class Starter {
         Service service = new Service();
         //String expression = "12-5+(1*5)-4*20";
         String expression = "12-5*((1+5)+8*40)-4*20";
+        //String expression = "5/5*5";
         System.out.println("Исходное выражение: "+ expression);
         double result = service.calc(expression);
         System.out.println("Результат: "+result);
