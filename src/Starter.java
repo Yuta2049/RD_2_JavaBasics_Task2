b2dfe08826adad2be735e8d7bd33f3686d026e12
Yuta2049 <vinci2049@gmail.com>
2018-11-19 11:09:06 +0400

    Decision via RegExp 3

--- src/Starter.java
+++ src/Starter.java
@@ -2,9 +2,12 @@ public class Starter {
 
     public static void main(String[] args) {
         Service service = new Service();
-        //String expression = "12-5+(1*5)-4*20";
-        String expression = "12-5*((1.5+5)+8*40)-4*20.5";   //12-
-        //String expression = "5/5*5";
+
+        String expression = "12-5*((1.5+5) + 8*40)-4*20,5";
+
+        expression = expression.replaceAll("\\s","");
+        expression = expression.replaceAll(",",".");
+
         System.out.println("Исходное выражение: " + expression);
         double result = service.calc(expression);
         System.out.println("Результат: " + result);
